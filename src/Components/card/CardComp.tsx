c9243bf1edc36cdfdcf40ab71e37c174cb788557
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-10-07 23:12:52 +0600

    add here new section

--- src/Components/card/CardComp.tsx
+++ src/Components/card/CardComp.tsx
@@ -1,25 +1,25 @@
 'use client'
-import { Card } from "flowbite-react"
+import { Button, Card } from "flowbite-react"
 import Image from "next/image"
 import React from 'react'
 
 const CardComp = () => {
   return (
       <>
-          <div className="max-w-xl drop-shadow-xl">
+          <div className="max-w-md sm:max-w-xl drop-shadow-xl">
               <Card >
                   <div className="flex gap-4 sm:gap-5 items-center">
-                        <Image src={`/images/card-image.png`} alt="Card Image " width={128} height={128} quality={100} className="w-32" />
+                        <Image src={`/images/card-image.png`} alt="Card Image " width={128} height={128} quality={100} className=" sm:w-32" />
                   <div>
-                          <p className="font-bold text-sm sm:text-lg/tight text-slate-700">209B Allen Jaja Street, Lagos Junction.</p>
+                          <p className="font-bold text-xs sm:text-lg/tight text-slate-700">209B Allen Jaja Street, Lagos Junction.</p>
                           <div className="flex  justify-between my-3 text-gray-500">
-                              <p>10 Bedroom </p>
-                              <p>150M </p>
-                              <p>3 Garage</p>
+                              <p className="text-xs sm:text-base">10 Bedroom </p>
+                              <p className="text-xs sm:text-base">150M </p>
+                              <p className="text-xs sm:text-base">3 Garage</p>
                           </div>
                           <div className="flex  justify-between items-center  ">
                               <p className="text-sm ">Posted by Simeon</p>
-                              <button className="bg-rose-600 text-white px-4 py-1.5 rounded-lg drop-shadow-2xl shadow-red-500/20 shadow-xl font-bold">$ 4550.5</button>
+                              <Button size={'xs'} color="failure" className=" text-white rounded-lg drop-shadow-2xl  shadow-red-500/20 shadow-xl font-bold">$ 4550.5</Button>
                           </div>
                   </div>
                 </div>
--- src/Section/Feature.tsx
+++ src/Section/Feature.tsx
@@ -5,21 +5,21 @@ import React from 'react'
 import pic from "../../public/images/features.png"
 import { Button } from 'flowbite-react'
 const Feature = () => {
-    const array = [1,2,3,4,5,6,7,8,9,10]
+    const array = [1,2,3,4]
   return (
-      <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 '>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-2   '>
           
           {
               array.map((e, i) => {
                   return (
                       <div key={i}>
-                             <div className='max-w-xl mx-auto'>
+                             <div className=' lg:max-w-2xl mx-auto'>
               <Card className=' drop-shadow-xl'>
                   <div>
                       <Image src={pic} alt='' width={1000} height={1200} className='drop-shadow-2xl' />
                       <div className='flex justify-between md:px-10 mt-2'>
                           <p className='text-xl md:text-3xl text-slate-500'><span className='text-slate-900 font-extrabold'>Kigali,</span> Kenya</p>
-                          <Button color='failure' size={'xs'}>Explore</Button>
+                          <Button color='failure' className='drop-shadow-2xl font-bold' size={'xs'}> <span className='drop-shadow-xl'>Explore</span> </Button>
                       </div>
                   </div>
               </Card>
